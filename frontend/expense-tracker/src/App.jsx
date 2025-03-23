7e7148a04ebbfed2fa68e2d200271c0007545b7a
Amit <amitchaurasia774@gmail.com>
2025-03-23 22:54:57 +0530

    add frontend files

---
+++ frontend/expense-tracker/src/App.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+
+import{
+      BrowserRouter as Router,
+      Routes,
+      Route,
+      Navigate,
+
+} from "react-router-dom";
+import Login from './pages/Auth/Login';
+
+import SignUp from './pages/Auth/SignUp';
+import Home from './pages/Dasboard/Home';
+import Income from './pages/Dasboard/Income';
+import Expense from './pages/Dasboard/Expense';
+
+function App() {
+  return (
+    <div >
+     <Router>
+      <Routes>
+        <Route path="/" element={< Root/>} />
+        <Route path="/login" element={<Login />} />
+        
+        
+        <Route path="/signup" element={<SignUp />} />
+         
+        <Route path="/dashboard" element={<Home />} />
+        
+        <Route path="/income" element={<Income />} />
+        
+        {/* Add more routes here */}
+        <Route path="/expense" element={<Expense />} />
+        
+      </Routes>
+     </Router> 
+
+    </div>
+  )
+}
+
+export default App
+
+const Root = () => {
+
+  const isAuthenticated = !!localStorage.getItem("token");
+
+  
+  return isAuthenticated ? (
+  <Navigate to="/dashboard" />
+) : (
+<Navigate to="/login" />);
+}
---
+++ frontend/expense-tracker/src/pages/Auth/SignUp.jsx
@@ -0,0 +1,114 @@
+import React, { useState } from 'react'
+import AuthLayout from '../../components/layouts/AuthLayout'
+import { Link, useNavigate } from 'react-router-dom';
+import Input from '../../components/Inputs/Input';
+import { validateEmail } from '../../utils/helper';
+import ProfilePhotoSelector from '../../components/Inputs/ProfilePhotoSelector';
+
+
+const SignUp= () => {
+
+const [profilePic , setProfilePic] = useState(null);
+const [fullName, setFullName] = useState("");
+const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
+const [error, setError] = useState(null);
+
+
+
+
+
+const navigate = useNavigate();
+
+
+const handleSignUp =async (e)=>{
+  
+  e.preventDefault();
+
+  let profileImageUrl="";
+  if(!fullName){
+    setError("Full Name is required");
+    return;
+  }
+  if(!validateEmail(email)){
+    setError("Invalid email address");
+    return;
+  }
+
+  if(!password){
+    setError("Password is required");
+    return;
+  }
+  
+
+  if(profilePic) {
+
+    profileImageUrl = await uploadProfileImage(profilePic);
+  }
+
+
+setError("");
+
+  navigate('/welcome');
+}
+
+  return (
+    <AuthLayout>
+     <div className='lg:w-[100%] h-auto md:h-full mt-10 md:mt-0 flex flex-col justify-center'>
+     <h3 className='text-xl font-semibold text-black'>Create an Account</h3>
+    <p className=' text-xs text-slate-700 mt-[5px] mb-6'>
+      Join us today by entering your details below to get started!. 
+    </p>
+
+    <form onSubmit={handleSignUp}>
+
+      <ProfilePhotoSelector image={profilePic} setImage={setProfilePic}/>
+
+
+
+
+
+
+
+       <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+        <Input
+          type="text"
+          placeholder="Full Name"
+          value={fullName}
+          onChange={({target}) => setFullName(target.value)}
+          label="Full Name"
+        />
+        <Input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={({target}) => setEmail(target.value)}
+          label="Email"
+        />
+
+        <div className='col-span-2'>
+        <Input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={({target}) => setPassword(target.value)}
+          label="Password"
+        />
+        </div>
+       </div>
+        {error && <p className='text-red-500 text-xs pb-2.5'>{error}</p>}
+       
+                   <button type="submit" className='mt-4 bg-primary text-white rounded px-4 py-2 w-full text-sm font-medium hover:bg-purple-600/15 hover:text-purple-600'>SignUp</button>
+       
+                   <p className='text-[13px] text-slate-800 mt-3'>
+                    Already have an account?{" "}
+                     <Link className=" font medium text-primary underline" to="/login">
+                     Login</Link>
+                   </p>
+       </form>
+       </div>
+    </AuthLayout>
+  )
+}
+
+export default SignUp
